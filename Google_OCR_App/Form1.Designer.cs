5b784bba31a683d0176005864007141a3c62f4f5
jshen <jshen@anglicaresa.com.au>
2017-08-15 15:14:54 +0930

    Add project files.

---
+++ Google_OCR_App/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Google_OCR_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBtn
+            // 
+            this.TextBtn.Location = new System.Drawing.Point(27, 56);
+            this.TextBtn.Name = "TextBtn";
+            this.TextBtn.Size = new System.Drawing.Size(199, 45);
+            this.TextBtn.TabIndex = 0;
+            this.TextBtn.Text = "TEXT_DOCUMENT_DETECTION";
+            this.TextBtn.UseVisualStyleBackColor = true;
+            this.TextBtn.Click += new System.EventHandler(this.TextBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 152);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(808, 57);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(27, 215);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox2.Size = new System.Drawing.Size(808, 129);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(27, 365);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(808, 251);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File: ";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(59, 19);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(517, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "C:\\\\Jerry Shen\\\\doc.JPG";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "TEXT_DETECTION";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(24, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(420, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "TEXT_DETECTION can recogize Chinese!! TEXT_DOCUMENT_DETECTION cannot.";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(507, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(328, 45);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "SpeechToText";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 644);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TextBtn);
+            this.Name = "Form1";
+            this.Text = "Google Cloud Vision and AI APIs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TextBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Google_OCR_App/Form1.cs
@@ -0,0 +1,576 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using static Newtonsoft.Json.JsonConvert;
+
+
+namespace Google_OCR_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void TextBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //get image into base64string
+                string imageFilePath = textBox4.Text.Trim();
+                FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+             
+                byte[] byteData = binaryReader.ReadBytes((int)fileStream.Length);
+                string base64String = Convert.ToBase64String(byteData);               
+                string image_base64string = base64String;
+
+                //generate json content string
+                string jsonstring_orig = @"{ 
+                      
+                     ""requests"": [
+                           {
+                                                 ""image"": {
+                                                               ""content"": ""rep_image_base64string""
+                                                            },
+
+                                               ""features"": [
+                                                            {
+                                                              ""type"": ""DOCUMENT_TEXT_DETECTION""
+                                                             }
+                                                            ],
+                                               ""imageContext"": { ""languageHints"": [""en"" ,""zh-CN"",""zh-TW"" ,""zh""]},
+
+
+                            }
+                    ]
+                   }";
+                string jsonstring = jsonstring_orig.Replace("rep_image_base64string", image_base64string);
+
+                textBox2.Text = jsonstring;
+
+                //new http client and call api
+                HttpClient client = new HttpClient();
+                var content = new StringContent(jsonstring, Encoding.UTF8, "application/json");
+                var url = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBf3aybUgE0aEvKgFRnBhZVN09V3S-A2js";
+                var response = await client.PostAsync(url, content);
+
+                var status = response.IsSuccessStatusCode;
+                if (status)
+                {
+
+                    string contentString = await response.Content.ReadAsStringAsync();
+                    string obj_descrpiton = "";
+                    var obj = DeserializeObject<RootObject>(JsonPrettyPrint(contentString));
+
+                    //Get description from textAnnotations
+                    foreach (var obj_response in obj.responses)
+                    {
+
+                        foreach (var obj_text in obj_response.textAnnotations)
+                        {
+                            if (obj_text.locale == "en" || obj_text.locale == "zh" || obj_text.locale == "nl")
+                            {
+
+                                obj_descrpiton += obj_text.description;
+                            }
+
+                        }
+
+
+
+                    }
+
+                    textBox3.Text = obj_descrpiton;
+
+                    textBox1.Text = "Call API Successfully!" ;
+                }
+                else
+                {
+                    textBox1.Text = "Call API failed !";
+
+                }
+
+
+            }
+            catch (Exception ee)
+            {
+
+                textBox1.Text = "Call API Error:" + ee.Message.ToString();
+            }
+
+        }
+
+        static string GetFromImageFile(string imageFilePath)
+        {
+
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+
+            byte[] byteData = binaryReader.ReadBytes((int)fileStream.Length);
+            string base64String = Convert.ToBase64String(byteData);
+            return base64String;
+
+
+
+
+        }
+        /// <summary>
+        /// Formats the given JSON string by adding line breaks and indents.
+        /// </summary>
+        /// <param name="json">The raw JSON string to format.</param>
+        /// <returns>The formatted JSON string.</returns>
+        static string JsonPrettyPrint(string json)
+        {
+            if (string.IsNullOrEmpty(json))
+                return string.Empty;
+
+            json = json.Replace(Environment.NewLine, "").Replace("\t", "");
+
+            StringBuilder sb = new StringBuilder();
+            bool quote = false;
+            bool ignore = false;
+            int offset = 0;
+            int indentLength = 3;
+
+            foreach (char ch in json)
+            {
+                switch (ch)
+                {
+                    case '"':
+                        if (!ignore) quote = !quote;
+                        break;
+                    case '\'':
+                        if (quote) ignore = !ignore;
+                        break;
+                }
+
+                if (quote)
+                    sb.Append(ch);
+                else
+                {
+                    switch (ch)
+                    {
+                        case '{':
+                        case '[':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', ++offset * indentLength));
+                            break;
+                        case '}':
+                        case ']':
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', --offset * indentLength));
+                            sb.Append(ch);
+                            break;
+                        case ',':
+                            sb.Append(ch);
+                            sb.Append(Environment.NewLine);
+                            sb.Append(new string(' ', offset * indentLength));
+                            break;
+                        case ':':
+                            sb.Append(ch);
+                            sb.Append(' ');
+                            break;
+                        default:
+                            if (ch != ' ') sb.Append(ch);
+                            break;
+                    }
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
+
+        public class Vertex
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+        }
+
+        public class BoundingPoly
+        {
+            public List<Vertex> vertices { get; set; }
+        }
+
+        public class TextAnnotation
+        {
+            public string locale { get; set; }
+            public string description { get; set; }
+            public BoundingPoly boundingPoly { get; set; }
+        }
+
+        public class DetectedLanguage
+        {
+            public string languageCode { get; set; }
+        }
+
+        public class Property
+        {
+            public List<DetectedLanguage> detectedLanguages { get; set; }
+        }
+
+        public class DetectedLanguage2
+        {
+            public string languageCode { get; set; }
+        }
+
+        public class Property2
+        {
+            public List<DetectedLanguage2> detectedLanguages { get; set; }
+        }
+
+        public class Vertex2
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+        }
+
+        public class BoundingBox
+        {
+            public List<Vertex2> vertices { get; set; }
+        }
+
+        public class DetectedLanguage3
+        {
+            public string languageCode { get; set; }
+        }
+
+        public class Property3
+        {
+            public List<DetectedLanguage3> detectedLanguages { get; set; }
+        }
+
+        public class Vertex3
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+        }
+
+        public class BoundingBox2
+        {
+            public List<Vertex3> vertices { get; set; }
+        }
+
+        public class DetectedLanguage4
+        {
+            public string languageCode { get; set; }
+        }
+
+        public class Property4
+        {
+            public List<DetectedLanguage4> detectedLanguages { get; set; }
+        }
+
+        public class Vertex4
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+        }
+
+        public class BoundingBox3
+        {
+            public List<Vertex4> vertices { get; set; }
+        }
+
+        public class DetectedLanguage5
+        {
+            public string languageCode { get; set; }
+        }
+
+        public class DetectedBreak
+        {
+            public string type { get; set; }
+        }
+
+        public class Property5
+        {
+            public List<DetectedLanguage5> detectedLanguages { get; set; }
+            public DetectedBreak detectedBreak { get; set; }
+        }
+
+        public class Vertex5
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+        }
+
+        public class BoundingBox4
+        {
+            public List<Vertex5> vertices { get; set; }
+        }
+
+        public class Symbol
+        {
+            public Property5 property { get; set; }
+            public BoundingBox4 boundingBox { get; set; }
+            public string text { get; set; }
+        }
+
+        public class Word
+        {
+            public Property4 property { get; set; }
+            public BoundingBox3 boundingBox { get; set; }
+            public List<Symbol> symbols { get; set; }
+        }
+
+        public class Paragraph
+        {
+            public Property3 property { get; set; }
+            public BoundingBox2 boundingBox { get; set; }
+            public List<Word> words { get; set; }
+        }
+
+        public class Block
+        {
+            public Property2 property { get; set; }
+            public BoundingBox boundingBox { get; set; }
+            public List<Paragraph> paragraphs { get; set; }
+            public string blockType { get; set; }
+        }
+
+        public class Page
+        {
+            public Property property { get; set; }
+            public int width { get; set; }
+            public int height { get; set; }
+            public List<Block> blocks { get; set; }
+        }
+
+        public class FullTextAnnotation
+        {
+            public List<Page> pages { get; set; }
+            public string text { get; set; }
+        }
+
+        public class Respons
+        {
+            public List<TextAnnotation> textAnnotations { get; set; }
+            public FullTextAnnotation fullTextAnnotation { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<Respons> responses { get; set; }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //get image into base64string
+                string imageFilePath = textBox4.Text.Trim();
+                FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+
+                byte[] byteData = binaryReader.ReadBytes((int)fileStream.Length);
+                string base64String = Convert.ToBase64String(byteData);
+                string image_base64string = base64String;
+
+                //generate json content string
+                string jsonstring_orig = @"{ 
+                      
+                     ""requests"": [
+                           {
+                                                 ""image"": {
+                                                               ""content"": ""rep_image_base64string""
+                                                            },
+
+                                               ""features"": [
+                                                            {
+                                                              ""type"": ""TEXT_DETECTION""
+                                                             }
+                                                            ],
+                                               ""imageContext"": { ""languageHints"": [""en"" ,""zh-CN"",""zh-TW"" ,""zh""]},
+
+
+                            }
+                    ]
+                   }";
+                string jsonstring = jsonstring_orig.Replace("rep_image_base64string", image_base64string);
+
+                textBox2.Text = jsonstring;
+
+                //new http client and call api
+                HttpClient client = new HttpClient();
+                var content = new StringContent(jsonstring, Encoding.UTF8, "application/json");
+                var url = "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBf3aybUgE0aEvKgFRnBhZVN09V3S-A2js";
+                var response = await client.PostAsync(url, content);
+
+                var status = response.IsSuccessStatusCode;
+                if (status)
+                {
+
+                    string contentString = await response.Content.ReadAsStringAsync();
+                    string obj_descrpiton = "";
+                    var obj = DeserializeObject<RootObject>(JsonPrettyPrint(contentString));
+
+                    //Get description from textAnnotations
+                    foreach (var obj_response in obj.responses)
+                    {
+
+                        foreach (var obj_text in obj_response.textAnnotations)
+                        {
+                              if (obj_text.locale =="en" || obj_text.locale == "zh" || obj_text.locale == "nl")
+                             {
+
+                            obj_descrpiton += obj_text.description;
+                              }
+
+                        }
+
+
+
+                    }
+
+                    textBox3.Text = obj_descrpiton;
+
+                    textBox1.Text = "Call API Successfully!";
+                }
+                else
+                {
+                    textBox1.Text = "Call API failed !";
+
+                }
+
+
+            }
+            catch (Exception ee)
+            {
+
+                textBox1.Text = "Call API Error:" + ee.Message.ToString();
+            }
+
+        }
+  
+
+        /// <summary>
+        /// call Google Speech to Text api
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //get audio file into base64string
+                //string speechFilePath = textBox4.Text.Trim();
+                //FileStream fileStream = new FileStream(speechFilePath, FileMode.Open, FileAccess.Read);
+                //BinaryReader binaryReader = new BinaryReader(fileStream);
+
+                //byte[] byteData = binaryReader.ReadBytes((int)fileStream.Length);
+                //string base64String = Convert.ToBase64String(byteData);
+                //string audio_base64string = base64String;
+
+
+                string audioFilePath = textBox4.Text.Trim();
+                FileStream fileStream = File.OpenRead(@audioFilePath);
+                MemoryStream memoryStream = new MemoryStream();
+                memoryStream.SetLength(fileStream.Length);
+                fileStream.Read(memoryStream.GetBuffer(), 0, (int)fileStream.Length);
+                byte[] BA_AudioFile = memoryStream.GetBuffer();
+                string base64String = Convert.ToBase64String(BA_AudioFile);
+                string audio_base64string = base64String;
+
+                //generate json content string
+                string jsonstring_orig = @"{
+                              ""config"": {
+                              ""encoding"": ""LINEAR16"",   
+                              ""sampleRateHertz"": 16000,                    
+                              ""languageCode"": ""en-US"",
+                              ""enableWordTimeOffsets"": false
+                                          },
+                             ""audio"": {
+                              ""content"": ""rep_aution""
+                             }
+                             }";
+
+                string jsonstring = jsonstring_orig.Replace("rep_aution", audio_base64string);
+
+                textBox2.Text = jsonstring;
+
+                File.WriteAllText("C:\\Jerry Shen\\google_text_dection.txt", jsonstring);
+
+                //new http client and call api
+                HttpClient client = new HttpClient();
+                var content = new StringContent(jsonstring, Encoding.UTF8, "application/json");
+                var url = "https://speech.googleapis.com/v1/speech:recognize?key=AIzaSyBf3aybUgE0aEvKgFRnBhZVN09V3S-A2js";
+                var response = await client.PostAsync(url, content);
+
+                var resutl = response.StatusCode;
+
+                var status = response.IsSuccessStatusCode;
+                if (status)
+                {
+
+                    string contentString = await response.Content.ReadAsStringAsync();
+                    string obj_descrpiton = "";
+                    var obj = DeserializeObject<RootObject_Audio>(JsonPrettyPrint(contentString));
+
+                    //Get description from textAnnotations
+                    foreach (var obj_response in obj.results)
+                    {
+
+                        foreach (var obj_alternativew in obj_response.alternatives)
+                        {
+                          
+                                obj_descrpiton += obj_alternativew.transcript;
+                        
+
+                        }
+
+
+
+                    }
+
+                    textBox3.Text = obj_descrpiton;
+
+                    textBox1.Text = "Call API Successfully!";
+                }
+                else
+                {
+                    textBox1.Text = "Call API failed !" + resutl.ToString(); ;
+
+                }
+
+
+            }
+            catch (Exception ee)
+            {
+
+                textBox1.Text = "Call API Error:" + ee.Message.ToString();
+            }
+        }
+
+        //calss for speech to text 
+        public class Alternative
+        {
+            public string transcript { get; set; }
+            public double confidence { get; set; }
+        }
+
+        public class Result
+        {
+            public List<Alternative> alternatives { get; set; }
+        }
+
+        public class RootObject_Audio
+        {
+            public List<Result> results { get; set; }
+        }
+    }
+}
